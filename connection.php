de73ea360f5397886c630090a720ae9caa5fe062
Honey K Kuriakose <50575878+HoneyKKuriakose27@users.noreply.github.com>
2021-06-08 18:42:50 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-  $conn=mysqli_connect('localhost','root','','honeyBPMS') or die('error connecting to mysql server'); //localhost
-   // $conn=mysqli_connect('remotemysql.com','ppywArkGLZ','enG8QtozM5','ppywArkGLZ') or die('error connecting to mysql server');
+  //$conn=mysqli_connect('localhost','root','','honeyBPMS') or die('error connecting to mysql server'); //localhost
+    $conn=mysqli_connect('remotemysql.com','ppywArkGLZ','enG8QtozM5','ppywArkGLZ') or die('error connecting to mysql server');
 ?>
